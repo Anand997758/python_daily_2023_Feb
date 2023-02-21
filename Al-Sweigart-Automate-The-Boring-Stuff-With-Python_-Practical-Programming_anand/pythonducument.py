5548508f2b0fdba239bc8ef85e1bb9d9aaf59d2e
Anand997758 <96283755+Anand997758@users.noreply.github.com>
2023-02-21 10:48:39 +0530

    20-02-2023

---
+++ Al-Sweigart-Automate-The-Boring-Stuff-With-Python_-Practical-Programming_anand/pythonducument.py
@@ -0,0 +1,42 @@
+#17-02-2023
+#============
+# print('hello, world')
+# '42' + 4
+#int
+# a = 1,2,3,4,5
+# print(a)
+#float
+# b = 1.2,1,4,4.5,2.0,4
+# print(b)
+#string
+# c = "an" + 'and'
+# print(c)
+# c = "hi" + 1
+# a = 'alice' * 5
+# print(
+
+# print("hello", "world!")
+# print("what is your name?")
+# myName = input()
+# print("my name is, " + myName)
+
+# print("my age is 25")
+# myAge = input()
+# print("my age is, " + str(int(myAge)) + 'in a year.')
+
+# print("tell me your name: ")
+# myName = input()
+# print(len(myName))
+
+# print("hello", "world")
+# print("what is your name")
+# print("A1")
+# myName = input()
+# print("it is good to meet, " + myName)
+
+# length = input()
+# print("the length of your name is: ", len(length))
+
+# print("what is your age?")
+# age = input()
+# print(age)
