b8461f82a9aff94010816e64c7e44490c600b1bf
Anand997758 <reddyanand480@gmail.com>
2023-02-23 12:13:59 +0530

    23-02-2023

---
+++ Al-Sweigart-Automate-The-Boring-Stuff-With-Python_-Practical-Programming_anand/practice_questions_python.py
@@ -0,0 +1,41 @@
+# 1. Which of the following are operators, and which are values? 
+# a. *
+# b. 'hello'
+# c. -88.8
+# d. -
+# e. /
+# f. +
+# d. 5
+# A. *, -, /, +
+
+# 2. Which of the following is a variable, and which is a string?
+# spam
+# 'spam'
+# A. spam = variable
+# 'spam' = is a string
+
+#3. name three data types?
+#A. int, float, boolian, string, list, tuple, dictionary
+
+#4. What is an expression made up of? What do all expressions do?
+#A. expressions will be do to combain the values 
+
+#5. This chapter introduced assignment statements, like spam = 10. What is 
+#the difference between an expression and a statement?
+
+#6. What does the variable bacon contain after the following code runs?
+#bacon = 20
+#bacon + 1
+#A. after run the the output will be come 21
+
+#7. What should the following two expressions evaluate to?
+#'spam' + 'spamspam'
+#'spam' * 3
+#A. This two expressions will be do 'spamspamspam
+#spamspamspam'
+
+#8. Why is eggs a valid variable name while 100 is invalid?
+#A. The varible first it will start with only names no numbers, so that's why 100 is invalid
+
+#9. what threee functions can be used to get the integer,floating-point number, or string version of a value?
+#Yes we can use to get the integer values we used the function in float(), and int()
--- Al-Sweigart-Automate-The-Boring-Stuff-With-Python_-Practical-Programming_anand/pythonducument.py
+++ Al-Sweigart-Automate-The-Boring-Stuff-With-Python_-Practical-Programming_anand/pythonducument.py
@@ -1,3 +1,4 @@
+#srikanth anna ipv6 2409:4072:713:3e68:0:0:16a8:38ad
 #17-02-2023
 #============
 # print('hello, world')
@@ -40,3 +41,204 @@
 # print("what is your age?")
 # age = input()
 # print(age)
+
+#string function()
+# a = str(24)
+# print(a)
+
+#21-02-2023
+#============
+# myAge = input()
+# print("i am , " + str(myAge) + 'years old.')
+
+# a = str(0)
+# print(a)
+
+# b = str(-0.12)
+# print(b)
+
+# c = int(58+1)
+# print(c)
+
+# d = int("281")
+# print(d)
+
+# e = int(-1.28)
+# print(e)
+
+# f = float('5')
+# print(f)
+
+# g = float(-1)
+# print(g)
+
+# h = float(1.8)
+# print(h)
+
+# spam = input()
+# print(spam)
+
+# print(spam)
+
+# spam = str(spam)
+# print(spam)
+
+# spam = input()
+# a = spam * 1010 / 2
+# print(a)
+
+# a = int('1.6')
+# print(a)
+
+# b = str('twelve')
+# print(b)
+
+# c = int(7.8)
+# print(c)
+
+# d = int(6.9) + 1
+# print(d)
+
+#22-02-2023
+#===============
+# print("what is your age?")
+# myAge = input()
+# print("my age is " + str(int(myAge) + 1) + 'in a year.')
+
+# value = input()
+# a = 42 == 42.0
+# print(a)
+
+# b = '43' == 43.0
+# print(b)
+
+# set = {1,2,2,4,5,4,5,6,7}
+# print(set)
+#
+
+#23-02-2023
+#===========
+# spam = True
+# print(spam)
+
+#comparision operater
+#=====================
+# a = 42==42
+# print(a)
+
+# b = 42==41
+# print(b)
+
+# c = 2 != 3
+# print(c)
+
+# d = 2 != 2
+# print(d)
+
+# e = 'value' == 'value' #true
+# print(e)
+
+# f = 'value' == 'value1' #false
+# print(f)
+
+# g = 'value' != 'value1' #true
+# print(g)
+
+# h = 'value' != 'value' #false
+# print(h)
+
+# i = 42 == 42.0 #true
+# print(i)
+
+# j = 42 == '42'
+
+# lessthan and greterthan (or) lessthanequalto and gretherthanequalto <,>,<=,>=
+# a = 42 < 45 #true
+# print(a)
+
+# b = 45 < 42
+# print(b)
+
+# c = 59 > 49 #true
+# print(c)
+
+# d = 47 > 68 #false
+# print(d)
+
+# eggcount = 42
+# print(eggcount)
+# eggcount <= 42
+# print(eggcount)
+
+#boolean operators:
+#=======================
+# a = True and True #true
+# print(a)
+
+# b = False and True #false
+# print(b)
+
+# c = True and False #false
+# print(c)
+
+# d = False and False #false
+# print(d)
+
+# a = True or False #true
+# print(a)
+
+# b = False or True #True
+# print(b)
+
+# c = True or True #true
+# print(c)
+
+# d = False or False #false
+# print(d)
+
+# a = not True
+# print(a)
+
+# b = not False
+# print(b)
+
+# c = not not True
+# print(c)
+
+#mixing boolean operators and comparision operators
+# a = (21 > 14) and (21 > 14) #true+true= true
+# print(a)
+
+# b = (14 < 13) and (13 < 15) #false+true=false
+# print(b)
+
+# c = (18 > 25) or (25 < 82) #false+true=true
+# print(c)
+
+# d = (4 < 5) and (5 < 6) #true+true=true
+# print(d)
+
+# e = (4 < 5) and (9 < 6) #true+false=false
+# print(e)
+
+# f = (1 == 2) or (2 == 2) #false+true
+# print(f)
+
+# g = (2 == 2) or (2 ==2) #true+true=true
+# print(g)
+
+# h = 2 + 2 == 4 and not 2 + 2 == 5 and 2*2 == 2 + 2 #true+false+falses=true
+# print(h)
+
+#if else conditions statements
+# name = 'anand'
+# password = 'anand@997758'
+# if name == 'anand':
+#     print('access granted')
+# else: 
+#     print('your user name is incorrect please try again')
+
+# if password == 'anand997758':
+#     print('access granted')
+# else:
+#     print('your password is incorrect, please try again')
