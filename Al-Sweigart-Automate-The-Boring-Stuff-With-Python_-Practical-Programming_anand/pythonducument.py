341cb02a8c468e9cc7fbb53db11f4225b0c9eb1e
Anand997758 <reddyanand480@gmail.com>
2023-02-27 17:13:58 +0530

    27-02-2023

--- Al-Sweigart-Automate-The-Boring-Stuff-With-Python_-Practical-Programming_anand/pythonducument.py
+++ Al-Sweigart-Automate-The-Boring-Stuff-With-Python_-Practical-Programming_anand/pythonducument.py
@@ -376,15 +376,253 @@
 # c = (92 > 90) or (97 < 96) #true+flase=true
 # print(c)
 
-name = ""
-while not name:
-    print('enter your name')
-    name = input()
-print('how many guests you have')
-numOfGuestes = int(input())
-if numOfGuestes:
-        print('be sure to enough room for all your guests')
-print('done')
+# name = ""
+# while not name:
+#     print('enter your name')
+#     name = input()
+# print('how many guests you have')
+# numOfGuestes = int(input())
+# if numOfGuestes:
+#         print('be sure to enough room for all your guests')
+# print('done')
 
+#For loop
+# print('hi anand')
+# for i in range(5):
+#     print('hi anand 5 times ('+ str (i) + ')')
 
+# print('what is your name')
+# total = 0
+# for i in range(100):
+#     print('print my name 5 times anand ('+ str(i) + ')')
+
+# print('hi anand')
+# i = 0
+# while i < 101:
+#     print('print anand 100 times('+ str(i) + ')')
+#     i = i + 1
+
+# for i in range(0,100):
+#     print(i)
+
+# for i in range(0,100,2):
+#     print(i)
+
+# for i in range(10, -1, -1):
+#     print(i)
+
+# for i in range(100,-1,-2):
+#     print(i)
+
+# print('tell me your name')
+# name = input()
+# print(len(name))
+
+# import random
+# for i in range(5):
+#     print(random.randint(1,10))
+
+# import sys
+# while True:
+#     print('type exit to exit')
+#     response = input()
+#     if response == 'exit':
+#         sys.exit()
+#     print('you typed  '+ response + '.')
+
+# print('type your name')
+# name = input()
+# while name != 'anand':
+#     break
+# print('your name write')
+
+# while True:
+#     print('how are you anand')
+#     name = input()
+#     if name != 'fine':
+#         continue
+#     print('hi anand, please enter your password')
+#     password = input()
+#     if password == 'anand@997758':
+#         break
+#     print('please login to console')
+    ## else:
+    ##     print('this not your password, please try again')
+    # #    break
+
+# import sys
+# while True:
+#     print('exit to exit')
+#     response = input()
+#     if response == 'exit':
+#         sys.exit()
+#     print('please type' + response + '.')
+
+# import random
+# secretNumber = random.randint(1,20)
+# print('i am thinking the number will be come between 1 to 20')
+
+# #ask the player to 6 times.
+# for gucesstaken in range(1,7):
+#     print('take a guess')
+#     guess = int(input())
+
+#     if guess > secretNumber:
+#         print('your gucesses is very low')
+#     elif guess > secretNumber:
+#         print('your guess is too high')
+
+#     else: 
+#         break #if the condition is correct guess
+
+#     if guess == secretNumber:
+#         print('Good job! You guessed my number in ' + str(guessesTaken) + ' guesses!')
+#     else:
+#         print('Nope. The number I was thinking of was ' + str(secretNumber))
+
+# import random
+# secretNumber = random.randint(1,20)
+
+# print('I am thinking the number will be come between 1 and 20')
+
+# for gusses in range():
+#     print('take a guess')
+#     guess = int(input())
+
+#     if guess < secretNumber:
+#         print('your guess too low')
+#     elif guess > secretNumber:
+#         print('your guess is too high')
+#     else:
+#         break #this condition is the correct
+#     if guess == secretNumber:
+#         print('Good job! you gussed my number ' + str(guessestaken) + 'gusses!')
+#     else: 
+#         print('Nope. The number i was thinking of was' + str(secretNumber))
+
+#27-02-2023
+#===========
+# import random, sys
+# print('ROCK, PAPER, SCISSORS')
+# # These variables keep track of the number of wins, losses, and ties.
+# wins = 0
+# losses = 0
+# ties = 0
+# while True: # The main game loop.
+#     print('%s Wins, %s Losses, %s Ties' % (wins, losses, ties))
+#     while True: # The player input loop.
+#         print('Enter your move: (r)ock (p)aper (s)cissors or (q)uit')
+#         playerMove = input()
+#         if playerMove == 'q':
+#             sys.exit() # Quit the program.
+#         if playerMove == 'r' or playerMove == 'p' or playerMove == 's':
+#             break # Break out of the player inp
+#         print('Type one of r, p, s, or q.')
+
+# import random, sys
+
+# print('ROCK, PAPER, SCISSORS')
+
+# # this variables keep tracking on wins, losses, ties
+# wins  = 0
+# losses = 0
+# ties = 0
+
+# while True:
+#     print('%s wins, %s losses, %s ties % (wins, losses, ties)')
+#     while True:
+#         print('Enter your move: (r)ock (p)aper (s)cissors (q)uit')
+#         playMove = input()
+#         if playMove == 'q':
+#             sys.exit() #quit the program
+#         if playMove == 'r' or playMove == 'p' or playMove == 's':
+#             break
+#         print('type one of r,p,s, or q.')
+
+#         if playMove == 'r':
+#             print('rock versus....')
+#         elif playMove == 'p':
+#             print('paper versus...')
+#         elif playMove == 's':
+#             print('scissors versus')
+
+#         #display what the computer choose
+#         randomNumber = random.randint(1,3)
+#         if randomNumber == 1:
+#             computerMove = 'r'
+#             print('rock')
+
+#         if randomNumber == 2:
+#             computerMove == 'p'
+#             print('paper')
+        
+#         if randomNumber == 3:
+#             computerMove == 's'
+#             print('scissors')
+
+#         #display and records the win/loss/tie.
+#         if playMove == computerMove:
+#             print('it a tie!')
+#             ties = ties + 1
+#         elif playMove == 'r' and playMove == 's':
+#             print('you win')
+#             wins = wins + 1
+#         elif playMove == 'p' and playMove == 'r':
+#             print('you win')
+#             wins = wins + 1
+#         elif playMove == 's' and playMove == 'p':
+#             print('you lose')
+#             loss = losses + 1
+#         elif playMove == 'r' and playMove == 'p':
+#             print('you lose')
+#             loss = losses + 1
+#         elif playMove == 's' and playMove == 'r':
+#             print('you lose!')
+#             loss = losses + 1
+# import sys
+# number = 1
+# if len(sys.argv) == 2:
+#     sys.argv[0]
+#     sys.argv[1]
+
+#Funtions
+# def hello():
+#     print('hello there')
+#     print('hello')
+#     print('hi')
+# hello()
+# hello()
+# hello()
+
+# a =60
+# if a < 20:
+#     print('this is false')
+# elif a > 80:
+#     print('yes this is correct number')
+# elif a > 70:
+#     print('please give me smaller number')
+# elif a > 40:
+#     print('this is perfect number, thank you')
+
+# else: 
+#     print('try again')
+
+#print last number
+# list = [2,4,3,5,53,35,36,21,1]
+# print('print the last number', list[-1])
+
+# print second highest number
+# list = [2,5,4,6,24,63,23,62,62]
+# list.sort()
+# print('give me second highest number',list[-2])
+
+# def hello(name):
+#     print('hello,' + name)
+# hello('Alice')
+# hello('Bob')
+
+# def hello(hi):
+#     print('anand,' + hi)
+# hello('say')
+# hello('bye')
 
